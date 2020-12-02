c2cf94d18f7cee9f8ad38af48d2a5ef1609e923f
odyssic <stevenbuddenjr@gmail.com>
2020-12-02 10:29:08 -0500

    top navbar

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -3,6 +3,7 @@ import { Link } from "react-router-dom";
 
 import MainHeader from "./MainHeader";
 import "./MainNavigation.css";
+import NavLinks from "./NavLinks";
 
 const Mainnavigation = (props) => {
   return (
@@ -15,7 +16,9 @@ const Mainnavigation = (props) => {
       <h1 className="main-navigation__title">
         <Link to="/">Your Places</Link>
       </h1>
-      <nav>...</nav>
+      <nav>
+        <NavLinks />
+      </nav>
     </MainHeader>
   );
 };
